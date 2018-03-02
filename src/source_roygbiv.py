d175f3ee49e9533fd1de1b3e8c4230cb921c3ba6
Matthew <sirtomato999@gmail.com>
2018-03-02 07:06:07 -0500

    A few tweaks to make life easier

--- src/source_roygbiv.py
+++ src/source_roygbiv.py
@@ -1,18 +1,19 @@
-import random
+import random, time
 
-colors = []
-colors.extend(('orange', 'red', 'blue', 'yellow', 'indigo', 'green', 'violet'))
+colors = ['orange', 'red', 'blue', 'yellow', 'indigo', 'green', 'violet']
 
 favColor = random.choice(colors)
 
 print("Hi! My favorite color is found in the rainbow.")
 
+time.sleep(1)
+
 GameRunning = True
 
 while GameRunning:
-	guess = raw_input("\nGuess my favorite color: ")
+	guess = raw_input("\nGuess my favorite color in the rainbow: ")
 
-	if guess != favColor:
+	if guess.lower() != favColor:
 		print("Wrong!")
 		print("Please, try again :)")
 	elif guess == favColor:
